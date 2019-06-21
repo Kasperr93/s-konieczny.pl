31a088e5e81cf38c5ab674112353a6fd94143a1b
Tomasz Kasperek <kasperek1993@gmail.com>
2019-06-22 01:18:05 +0200

    added function to close mobile nav on click

--- custom.js
+++ custom.js
@@ -1,10 +1,23 @@
 /* ============ NAVBAR TRANSPARENT TO SOLID ============ */
 $(document).ready(function() {
 	$(window).scroll(function() {
-		if($(this).scrollTop() > 300) {
+
+		if($(this).scrollTop() > 300) { 
 			$('.navbar').addClass('solid');
 		} else {
-			$('.navbar').removeClass('solid');
+		$('.navbar').removeClass('solid');
+		}
+	});
+});
+
+/* ============ CLOSE MOBILE NAV ON CLICK ============ */
+$(document).ready(function() {
+	$(document).click(function(event) {
+		var clickOver = $(event.target);
+		var _opened = $(".navbar-collapse").hasClass("show");
+
+		if (_opened === true && !clickOver.hasClass("navbar-toggler")) {
+			$(".navbar-toggler").click();
 		}
 	});
 });
